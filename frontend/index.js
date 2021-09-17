3c3994ac896af8e8219941bba8314c8178835147
Victors1967 <victorsmirnov@me.com>
2021-09-17 09:58:21 +0300

    add frontend

---
+++ frontend/index.js
@@ -0,0 +1,9 @@
+// frontend/index.js
+
+const express = require("express");
+
+app = express();
+
+app.listen(process.env.APP_FRONTEND_PORT);
+
+app.get("*", (req, res) => res.send("<h1>Hello, World!</h1>"));
